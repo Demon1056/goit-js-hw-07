5f1d8dc229921363120b0c416efb7c81fdfe730a
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2022-08-18 23:27:03 +0300

    start to work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryHtml= galleryItems.map(creatOneImage).join('')
+
+function creatOneImage ({preview, original, description}){
+    return `<div class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </div>`; 
+}
+
+const galleryArea= document.querySelector ('.gallery')
+galleryArea.insertAdjacentHTML('beforeend', galleryHtml)
