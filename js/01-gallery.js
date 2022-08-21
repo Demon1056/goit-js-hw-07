b6c0807549184143847dba5f30caac8d99bfe2d9
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2022-08-21 20:18:50 +0300

    Finish all tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
-
 const galleryHtml= galleryItems.map(creatOneImage).join('')
 
 function creatOneImage ({preview, original, description}){
     return `<div class="gallery__item">
-   <a class="gallery__link" href="large-image.jpg">
+   <a class="gallery__link" href = "large-image.jpg">
      <img
        class="gallery__image"
        src="${preview}"
        data-source="${original}"
        alt="${description}"
      />
-   </a>
+ </a>
  </div>`; 
 }
 
+
 const galleryArea= document.querySelector ('.gallery')
-galleryArea.insertAdjacentHTML('beforeend', galleryHtml)
+
+function createGallery() {
+  return galleryArea.insertAdjacentHTML('beforeend', galleryHtml)
+}
+createGallery()
+
+galleryArea.addEventListener('click', getBigImage )
+
+function getBigImage(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+  <img src= ${event.target.dataset.source} >
+  `)
+  instance.show ()
+  }
+  
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryHtml= galleryItems.map(creatOneImage).join('')
+
+function creatOneImage ({preview, original, description}){
+    return `<a class="gallery__item" href = "${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+     />
+ </a>
+ `; 
+}
+const galleryArea= document.querySelector ('.gallery')
+function createGallery() {
+  return galleryArea.insertAdjacentHTML('beforeend', galleryHtml)
+}
+createGallery()
+
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData:'alt', captionDelay:250} );
